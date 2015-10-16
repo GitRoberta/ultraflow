c1531089b6ac4ec7a26d683321ce41ac4227647f
Roberta Rappazzo <robertarappazzo@gmail.com>
2015-10-16 17:56:22 +0200

    player change

---
+++ ultraflowExercise/Assets/Scripts/Player.cs
@@ -0,0 +1,25 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Player : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        if (Input.touchCount == 1) {
+            float x = 0, y=0;
+            if (Input.touches[0].deltaPosition.x > -3 && Input.touches[0].deltaPosition.x < 3) {
+                x = Input.touches[0].deltaPosition.x;
+            }
+            if (Input.touches[0].deltaPosition.y > -3 && Input.touches[0].deltaPosition.y < 5)
+            {
+                y = Input.touches[0].deltaPosition.y;
+            }
+            transform.Translate(x,y, 0);
+        }
+	}
+}
