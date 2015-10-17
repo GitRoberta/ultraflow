d8ede1f9a73da4b65c92b75286336e2be4e1cea3
Chetoplez <frusso.d1989@gmail.com>
2015-10-17 20:04:02 +0200

    DeathArea

---
+++ ultraflowExercise/Assets/Scripts/DeathArea.cs
@@ -0,0 +1,17 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class DeathArea : EffectArea{
+
+    bool dead = false;
+
+    public override void OnTriggerStay2D(Collider2D other)
+    {
+        if (is_player(other) && !dead)
+        {
+            Player p = other.gameObject.GetComponent<Player>();
+            p.Amount = 0;
+            dead = true;
+        }
+    }
+}
