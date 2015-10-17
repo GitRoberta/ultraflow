1e479fabcef592f50c63c7141296b1cfd4bc9c8a
Chetoplez <frusso.d1989@gmail.com>
2015-10-17 17:16:35 +0200

    PlayerCorretto

---
+++ ultraflowExercise/Assets/Scripts/GameController.cs
@@ -0,0 +1,22 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class GameController : MonoBehaviour {
+
+    private int level_completed = 0; /* Once we complete a level, this variable */
+    public int Level_Completed {
+        get { return level_completed; }
+        set { if (value > level_completed) level_completed = value; }
+    }
+
+    void Awake()
+    {
+        DontDestroyOnLoad(transform.gameObject);
+    }
+
+	void Start () {
+	    
+	}
+	
+	
+}
