bdbf667dfbe89b5f0652c4c998f0a39852a3e1b7
Roberta Rappazzo <robertarappazzo@gmail.com>
2015-10-17 13:51:10 +0200

    Revert "Commit Rob"
    This reverts commit a2d455c63d32e129782f7d76d307150d50601616.

--- ultraflowExercise/Assets/Scripts/LevelHandler.cs
+++ ultraflowExercise/Assets/Scripts/LevelHandler.cs
@@ -4,38 +4,38 @@ using System.Collections;
 public class LevelHandler : MonoBehaviour
 {
  
- //   public GameObject MovableObject;      /* Pass the empty that contains all the moving platform on the game, if null nothing happens */
- //   public GameObject DestroyableObjects; /* Pass the empty that contains all the destroyable objects on the game, if null nothing happens */
- //   public GameObject player;
+    public GameObject MovableObject;      /* Pass the empty that contains all the moving platform on the game, if null nothing happens */
+    public GameObject DestroyableObjects; /* Pass the empty that contains all the destroyable objects on the game, if null nothing happens */
+    public GameObject player;
     
- //   [Range(1,20)]
- //   public int start_amount = 5;
+    [Range(1,20)]
+    public int start_amount = 5;
 
-	//void Start () {
- //       if (player == null)
- //       {
- //           Debug.LogError("LevelHandler: player is null. I kill myself");
- //           GameObject.Destroy(this.gameObject);
- //       }
-	//}
+	void Start () {
+        if (player == null)
+        {
+            Debug.LogError("LevelHandler: player is null. I kill myself");
+            GameObject.Destroy(this.gameObject);
+        }
+	}
 	
 	
-	//void Update () {
+	void Update () {
 	
-	//}
+	}
 
- //   /* Reset all elements of the level. */
- //   void reset() 
- //   {
- //       Wall[] d_walls = DestroyableObjects.GetComponentsInChildren<Wall>(true);
- //       foreach (Wall w in d_walls)
- //       {
- //           if (!w.gameObject.activeSelf)
- //               w.gameObject.SetActive(true);
- //           w.reset();
- //       }
- //       Player p = player.GetComponent<Player>();
- //       p.reset();
- //   }
+    /* Reset all elements of the level. */
+    void reset() 
+    {
+        Wall[] d_walls = DestroyableObjects.GetComponentsInChildren<Wall>(true);
+        foreach (Wall w in d_walls)
+        {
+            if (!w.gameObject.activeSelf)
+                w.gameObject.SetActive(true);
+            w.reset();
+        }
+        Player p = player.GetComponent<Player>();
+        p.reset();
+    }
 
 }
--- ultraflowExercise/Assets/Scripts/SlowArea.cs
+++ ultraflowExercise/Assets/Scripts/SlowArea.cs
@@ -5,42 +5,26 @@ using System.Collections;
 public class SlowArea : EffectArea {
     public AudioSource speed_sound;
 
-    //public override void Start()
-    //{
-    //    speed_sound = GetComponent<AudioSource>();
-    //}
+    public override void Start()
+    {
+        speed_sound = GetComponent<AudioSource>();
+    }
 
-    //public override void Update()
-    //{
+    public override void Update()
+    {
 
-    //}
+    }
 
-<<<<<<< HEAD
-    //public override void OnTriggerEnter2D(Collider2D other)
-    //{
-    //    if (!is_player(other)) return;
-    //    Player p = other.GetComponent<Player>() ?? null;
-    //    if (p != null)
-    //    {
-    //        p.Velocity /= speed_factor;
-    //        if (!speed_sound.isPlaying)
-    //            speed_sound.Play();
-    //    }
-    //}
-=======
     public override void OnTriggerEnter2D(Collider2D other)
     {
         if (!is_player(other)) return;
         Player p = other.GetComponent<Player>() ?? null;
         if (p != null)
         {
-            float angle = Vector3.Angle(p.gameObject.transform.up, this.transform.up);
-            p.transform.Rotate(-this.transform.forward, angle);
             p.Velocity /= speed_factor;
             if (!speed_sound.isPlaying)
                 speed_sound.Play();
         }
     }
->>>>>>> d57fc2fbdb14cefec207096215807b05182a99a9
 	
 }
