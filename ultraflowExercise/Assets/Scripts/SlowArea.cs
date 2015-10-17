d57fc2fbdb14cefec207096215807b05182a99a9
Chetoplez <frusso.d1989@gmail.com>
2015-10-17 12:56:30 +0200

    Correzione Direzione EffectArea

--- ultraflowExercise/Assets/Scripts/SlowArea.cs
+++ ultraflowExercise/Assets/Scripts/SlowArea.cs
@@ -21,6 +21,8 @@ public class SlowArea : EffectArea {
         Player p = other.GetComponent<Player>() ?? null;
         if (p != null)
         {
+            float angle = Vector3.Angle(p.gameObject.transform.up, this.transform.up);
+            p.transform.Rotate(-this.transform.forward, angle);
             p.Velocity /= speed_factor;
             if (!speed_sound.isPlaying)
                 speed_sound.Play();
