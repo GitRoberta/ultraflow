702389c528d6d28c6fa7f04b16b64063b5454da7
Roberta Rappazzo <robertarappazzo@gmail.com>
2015-10-17 13:56:12 +0200

    Commit Corretto

--- ultraflowExercise/Assets/Scripts/Player.cs
+++ ultraflowExercise/Assets/Scripts/Player.cs
@@ -1,25 +1,21 @@
 ﻿using UnityEngine;
 using System.Collections;
 
-public class Player : MonoBehaviour {
+public class PlayerHandler: MonoBehaviour {
 
-	// Use this for initialization
-	void Start () {
+    private Vector2 startPos;
+    private Vector2 direction;
+    private bool directionChosen;
+    private Vector3 dir;
+    private float velocity;
+
+    // Use this for initialization
+    void Start () {
 	
 	}
 	
 	// Update is called once per frame
 	void Update () {
-        if (Input.touchCount == 1) {
-            float x = 0, y=0;
-            if (Input.touches[0].deltaPosition.x > -3 && Input.touches[0].deltaPosition.x < 3) {
-                x = Input.touches[0].deltaPosition.x;
-            }
-            if (Input.touches[0].deltaPosition.y > -3 && Input.touches[0].deltaPosition.y < 5)
-            {
-                y = Input.touches[0].deltaPosition.y;
-            }
-            transform.Translate(x,y, 0);
-        }
-	}
+        
+    }
 }
