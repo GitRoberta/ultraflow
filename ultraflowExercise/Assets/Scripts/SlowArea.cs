441d5c12bdda2588b20565c58d4ca6971d016797
Chetoplez <frusso.d1989@gmail.com>
2015-10-18 10:32:01 +0200

    Third Level e correzione SlowArea

--- ultraflowExercise/Assets/Scripts/SlowArea.cs
+++ ultraflowExercise/Assets/Scripts/SlowArea.cs
@@ -10,21 +10,28 @@ public class SlowArea : EffectArea {
         speed_sound = GetComponent<AudioSource>();
     }
 
-    public override void Update()
-    {
-
-    }
+    
 
     public override void OnTriggerEnter2D(Collider2D other)
     {
         if (!is_player(other)) return;
         Player p = other.GetComponent<Player>() ?? null;
         if (p != null)
-        {
-            p.Velocity /= speed_factor;
-            if (!speed_sound.isPlaying)
-                speed_sound.Play();
-        }
+            speed_down(p);
+    }
+
+    public override void OnTriggerStay2D(Collider2D other)
+    {
+        if (!is_player(other)) return;
+        Player p = other.GetComponent<Player>() ?? null;
+        if (p != null)
+            speed_down(p);
+    }
+
+    private void speed_down(Player player) {
+        player.Velocity /= speed_factor;
+        if (!speed_sound.isPlaying)
+            speed_sound.Play();
     }
 	
 }
