a505d4eff6c60f1259ea8e015adac69e25147e9d
Chetoplez <frusso.d1989@gmail.com>
2015-10-18 16:50:16 +0200

    Particle Part 1

---
+++ ultraflowExercise/Assets/Scripts/Fragment.cs
@@ -0,0 +1,22 @@
+﻿using UnityEngine;
+using System.Collections;
+
+[RequireComponent(typeof(SpriteRenderer))]
+public class Fragment : MonoBehaviour {
+    
+    /* Velocity parameters */
+    [Range(1,10)]
+    public int start_speed = 3;
+    private Vector3 direction;
+
+    public int fragment_life = 3;
+
+	
+	void Start () {
+        direction = new Vector3(Random.value,Random.value,Random.value);
+	}
+	
+	void Update () {
+        transform.Translate(direction* Time.deltaTime* start_speed);
+	}
+}
